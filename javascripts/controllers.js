104d0c2ab4c619daced0deb1ad20426491ab97ac
SimonDolph <simon.dong.sea@hotmail.com>
2014-03-02 15:41:18 +0800

    add disqus

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -6,6 +6,14 @@ var blogControllers = angular.module('blogControllers', []);
 
 blogControllers.controller('PostListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('posts/index.json').success(function(data) {
+        DISQUS.reset({
+            reload: true,
+            config: function () {
+                this.page.identifier = 'blog home';
+                this.page.url = 'http://simondolph.github.io/#/posts';
+                this.page.title = 'Blog Home';
+            }
+        });
         $scope.posts = data;
     });
 }]);
@@ -17,6 +25,14 @@ blogControllers.controller('PostDetailCtrl', ['$scope', '$http', '$routeParams',
         }
 	});
     $http.get('posts/' + $routeParams.postID + '.md').success(function(data) {
+        DISQUS.reset({
+            reload: true,
+            config: function () {
+                this.page.identifier = $routeParams.postID;
+                this.page.url = 'http://simondolph.github.io/#/posts/' + $routeParams.postID;
+                this.page.title = $routeParams.postID;
+            }
+        });
         $scope.post = marked(data);
     });
 }]);
